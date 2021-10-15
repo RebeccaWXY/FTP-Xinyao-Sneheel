8a96a63e7fb89f026164e95c07daa138b65b6eb2
RebeccaWXY <58260839+RebeccaWXY@users.noreply.github.com>
2021-10-15 21:19:56 +0400

    Update client.c

--- client.c
+++ client.c
@@ -9,46 +9,44 @@
 int main(int argc, char *argv[])
 {
 	
-	//socket
-	int srv_socket = socket(AF_INET,SOCK_STREAM,0);	
-	struct sockaddr_in srv_addr;			//structure to hold the type, address and port
-	memset(&srv_addr,0,sizeof(srv_addr));	//set the Fill the structure with 0s
-	srv_addr.sin_family = AF_INET;			//Address family		
-	srv_addr.sin_port = htons(5000);				//Port Number
-	srv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // intead of all local onterfaces you can also specify a single enterface e.g. inet_addr("127.0.0.1") for loopback address
-	//connect
-	if(connect(srv_socket,(struct sockaddr*) &srv_addr,sizeof(srv_addr))==-1)
+	//1. socket();
+	int server_fd = socket(AF_INET,SOCK_STREAM,0);
+	printf("server_fd = %d \n",server_fd);
+	if(server_fd<0)
 	{
-		perror("Connect: ");
-		return -1;
+		perror("socket");
+		return 0;
 	}
-	char filename[50];
-	//rcv
-	if(argc<2)
-	{
-		printf("Enter the file name: ");
-		scanf("%s",filename);
-	}
-	else
-		strcpy(filename,argv[1]);
 
-	send(srv_socket,filename,strlen(filename),0);
-	printf("Requesting file %s \n",filename);
-	char buffer[1500];
-	int bytes;
-	FILE *fptr;
-	if(!(fptr = fopen(filename,"w")))
-		perror("Cant create file");
-	else
+	//2. connect ();
+	struct sockaddr_in server_address;
+	bzero(&server_address,sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(5000);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if(connect(server_fd,(struct sockaddr*)&server_address,sizeof(server_address))<0)
 	{
-		do
-		{
-			bytes = recv(srv_socket,buffer,sizeof(buffer),0);
-			if(bytes>0)
-				fwrite(buffer,bytes,1,fptr);
-		}while(bytes>0);
-		fclose(fptr);
+		perror("connect");
+		return 0;
 	}
-	close(srv_socket);
+	//3. send/recv
+	while(1)
+	{
+		char user_input[100];
+		fgets(user_input,100,stdin);
+		user_input[strcspn(user_input,"\n")]=0;   //remove new line char from string by replacing it with '\0'
+
+		send(server_fd,user_input,strlen(user_input),0); 
+		
+		if(strcmp(user_input,"bye")==0)
+			break;
+	}
+
+
+	//4. close
+	close(server_fd);
+
 	return 0;
+	
 }
