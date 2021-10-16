708a2b94493cef545a2cf839eb57f62e9f4279e4
RebeccaWXY <58260839+RebeccaWXY@users.noreply.github.com>
2021-10-17 01:57:38 +0400

    Update client.c

--- client.c
+++ client.c
@@ -5,6 +5,9 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<dirent.h>
 
 #define MAX_LENGTH 200
 
@@ -58,10 +61,13 @@ int main(int argc, char** argv)
 	//While ( 1 ) {
 		printf("ftp>");
 		//show ftp>
+
 		fgets(input_total, MAX_LENGTH, stdin);
 		//fgets a ftp command line from keyboard
 		sscanf(input_total, "%s %s", input_command, input_parameters);
-		
+		printf("input_command:%s\n", input_command);
+		printf("input_parameters:%s\n", input_parameters);
+
 		if (strcmp(input_command, "user")==0){
 		//if the command is "user username ”
 			send(srv_socket, input_total, MAX_LENGTH,0);
@@ -82,11 +88,48 @@ int main(int argc, char** argv)
 			//is successfully executed and display it to the user
 		}
 		
+		else if (strcmp(input_command, "put")==0){
+		//if the command is "put a existed file"
+			send(srv_socket, input_total, MAX_LENGTH,0);
+			//0 . send the command to the server
+			int src = open(input_parameters, O_RDONLY);
+			//1 . open the file
+			if (src<0){
+				printf("Error occurs when open file: %s\n", input_parameters);
+			}
+			//2 . read the file
+	
+			//3 . open a new TCP connection to server
+	
+			//3 . write the file to server
+	
+			//4 . close the file
+	
+			//5 . close the TCP connection
+		}
+
+		/*
+		if(){
+		//if the command is "get a file "
+	
+			//1 . send the command to the server
+	
+			//2 . fgets first line from the server : existed or non−existed
+	
+			//3 . if existed
 		
+				//3. 1 open a new TCP connection to server
 		
+				//3. 1 read the file from the server
 		
-		/*codes to be filled: put, get*/
+				//3. 2 write the file to the local directory
 		
+				//3. 3 close the new TCP connection
+	
+			//4 . if non−existed display "file name : no such file on server"
+		}
+		*/
+
 		
 		else if (strcmp(input_command, "cd")==0 || strcmp(input_command, "ls")==0 ||strcmp(input_command, "pwd")==0){
 		//if the command is "cd . . . ", "ls . . .", or "pwd"
@@ -158,8 +201,9 @@ int main(int argc, char** argv)
 			printf("An invalid ftp command.\n");
 			//otherwise : show "An invalid ftp command."		
 		}
-		
+
 	}
 	return 0;
 }
+
 
